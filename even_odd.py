ad45eb3cf8953dbcb8419c6f7fd955fe231d54e2
tripathi4251 <tripathi.4251@gmail.com>
2026-03-05 00:51:52 +0530

    initial commit

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+#Take an integer fromuser and check whether it is even or odd
+
+num=int(input("first integer: "))
+rem =num%2
+if (rem==0):
+    print("even")
+else:
+    print("odd")
---
+++ positive_negative.py
@@ -0,0 +1,9 @@
+#take a number from user and check whether the number is positive or negative
+
+num=int(input("first number:"))
+if (num>0):
+    print("number is positive")
+elif (num<0):
+    print("number is negative")
+else:
+    print("zero")
